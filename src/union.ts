6c36d47be76263be74ba859774e212dcfbbe4ecf
Szilard Hajba <szilu@symbion.hu>
2021-08-20 19:08:56 +0200

    union typing bugfix

--- src/union.ts
+++ src/union.ts
@@ -1,9 +1,11 @@
 import { Result, err, isOk } from './utils'
 import { Type, DecoderOpts, DecoderError, decoderError } from './type'
 
+type ElementType<T extends ReadonlyArray<unknown>> = T extends ReadonlyArray<infer ElementType> ? ElementType : never
+
 // Union //
 ///////////
-class UnionType<T extends ReadonlyArray<unknown>> extends Type/*BaseDecoder*/<T[number]> {
+class UnionType<T extends ReadonlyArray<unknown>> extends Type/*BaseDecoder*/<ElementType<T>> {
 	members: { [K in keyof T]: Type<T[K]> }
 
 	constructor(members: { [K in keyof T]: Type<T[K]> }) {
@@ -15,21 +17,21 @@ class UnionType<T extends ReadonlyArray<unknown>> extends Type/*BaseDecoder*/<T[
 		return this.members.map(member => member.print()).join(' | ')
 	}
 
-	decode(u: unknown, opts: DecoderOpts): Result<T[number], DecoderError> {
+	decode(u: unknown, opts: DecoderOpts): Result<ElementType<T>, DecoderError> {
 		let errors: string[] = []
 
 		for (const m of this.members) {
 			const matched = m.decode(u, opts)
 			if (isOk(matched)) {
-				return matched
+				return matched as Result<ElementType<T>, DecoderError>
 			}
 		}
 		return decoderError([], `non of the union type members matched`)
 	}
 }
 
-export function union<T extends ReadonlyArray<unknown>>(...members: { [K in keyof T]: Type<T[K]> }): Type<T[number]> {
-	return new UnionType(members)
+export function union<T extends ReadonlyArray<unknown>>(...members: { [K in keyof T]: Type<T[K]> }): Type<ElementType<T>> {
+	return new UnionType<T>(members)
 }
 
 // vim: ts=4
